858b0ec5424de71c86b5ef1192dba8da4804cac7
T-Galante <tales.galante@gmail.com>
2019-07-24 15:30:45 -0300

    Tales_Changing the correios_variables.py file

--- Variables/Correios_Variables.py
+++ Variables/Correios_Variables.py
@@ -2,7 +2,7 @@ __author__ = 'Brito'
 
 correios_url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/BuscaCepEndereco.cfm'
 
-correios_valid_zip = '13030130'
+correios_valid_zip = '13465289'
 correios_valid_address = 'Rua Alcindo Vieira Barreiro Belo Horizonte'
 
 result_zip_search = 'Rua Augusto de Carvalho Asbahr '
